2fb1664e0e93beb375e2a5f355f968fe71188e91
joaohfrancasilva <joao.franca@maisunifacisa.com.br>
2026-05-28 16:27:05 -0300

    Versão final do projeto

--- ProjetoLoja.java
+++ ProjetoLoja.java
@@ -8,20 +8,24 @@ public class ProjetoLoja{
     public static void main(String[] args){
         ArrayList<HashMap> lista = new ArrayList<>();
         int opcao = 0;
+        int id = 1;
         do{
             String[] menu = {
                     "01. Cadastrar novo produto",
                     "02. Listar todos os produtos",
                     "03. Buscar produtos pelo nome",
+                    "04. Registrar entrada no estoque (Compra)",
+                    "05. Registrar sáida de estoque (Venda)",
+                    "06. Excluir produto do sistema",
                     "07. Listar produtos com estoque zerado",
                     "08. Calcular valor total do estoque",
-                    "09 Atualizar preço do produto",
-                    "10. Sair"};
+                    "09. Atualizar preço do produto",
+                    "10. Sair"
+            };
             try {
                 String opcaoSTR = ((String) JOptionPane.showInputDialog(null, "Escolha uma opção", "Sistema da loja",
                         JOptionPane.QUESTION_MESSAGE, null, menu, menu[0])).substring(0,2);
                 opcao = Integer.parseInt(opcaoSTR);
-
 
                 switch (opcao){
                     case 1:
@@ -33,7 +37,6 @@ public class ProjetoLoja{
                                 break;
                             }
 
-
                             try{
                                 String precoStr = JOptionPane.showInputDialog(null, "Digite o preço do produto: ");
                                 double preco = Double.parseDouble(precoStr);
@@ -41,15 +44,14 @@ public class ProjetoLoja{
                                 produto.put("Nome", nome);
                                 produto.put("Preço", preco);
                                 produto.put("Quantidade no estoque", 0);
-                                produto.put("ID", (lista.size() + 1));
+                                produto.put("ID", id);
                                 lista.add(produto);
                                 JOptionPane.showMessageDialog(null, "Produto adicionado!");
+                                id++;
                             }
                             catch (NumberFormatException e) {
                                 JOptionPane.showMessageDialog(null, "Valor Inválido.");
                             }
-
-
                         }
                         catch (NullPointerException e){
                             JOptionPane.showMessageDialog(null, "Voltando ao menu principal.");
@@ -81,21 +83,29 @@ public class ProjetoLoja{
                         }
                         else {
                             boolean achou = false;
-                            String busca = (JOptionPane.showInputDialog(null, "Nome do produto: ")).trim();
-                            for (HashMap item : lista) {
-                                if (item.containsValue(busca)) {
-                                    JOptionPane.showMessageDialog(null,
-                                            "ID: " + item.get("ID") + "\n" +
-                                                    "Nome: " + item.get("Nome") + "\n" +
-                                                    "Preço: R$" + String.format("%.2f\n", (double) item.get("Preço")) +
-                                                    "Quantidade no estoque: " + item.get("Quantidade no estoque"));
-                                    if(!achou){
-                                        achou = true;
+                            String busca = JOptionPane.showInputDialog(null, "Nome do produto: ");
+                            if (busca == null) {
+                                JOptionPane.showMessageDialog(null, "Voltando ao menu principal.");
+                            }
+                            else if (busca.isBlank()){
+                                JOptionPane.showMessageDialog(null, "Valor inválido.");
+                            }else {
+                                for (HashMap item : lista) {
+                                    String nomeProduto = (String) item.get("Nome");
+                                    if (nomeProduto.toLowerCase().contains(busca.toLowerCase())) {
+                                        JOptionPane.showMessageDialog(null,
+                                                "ID: " + item.get("ID") + "\n" +
+                                                        "Nome: " + item.get("Nome") + "\n" +
+                                                        "Preço: R$" + String.format("%.2f\n", (double) item.get("Preço")) +
+                                                        "Quantidade no estoque: " + item.get("Quantidade no estoque"));
+                                        if (!achou) {
+                                            achou = true;
+                                        }
                                     }
                                 }
-                            }
-                            if (!achou) {
-                                JOptionPane.showMessageDialog(null, "Não há nenhum produto com esse nome.");
+                                if (!achou) {
+                                    JOptionPane.showMessageDialog(null, "Não há nenhum produto com esse nome.");
+                                }
                             }
                         }
                         break;
@@ -103,8 +113,126 @@ public class ProjetoLoja{
 
 
                     case 4:
+                        if (lista.isEmpty()) {
+                            JOptionPane.showMessageDialog(null, "A lista está vazia.");
+                        }
+                        else {
+                            try {
+                                int idBusca = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o ID do produto:"));
+                                boolean achou = false;
+                                for (HashMap item : lista) {
+                                    if ((int) item.get("ID") == idBusca) {
+                                        achou = true;
+                                        int quantidade = Integer.parseInt(
+                                                JOptionPane.showInputDialog(null, "Quantidade adquirida:")
+                                        );
+                                        int estoqueAtual = (int) item.get("Quantidade no estoque");
+                                        item.put("Quantidade no estoque", estoqueAtual + quantidade);
+                                        JOptionPane.showMessageDialog(null,
+                                                "Entrada registrada com sucesso.\n" +
+                                                        "Novo estoque: " + item.get("Quantidade no estoque"));
+                                        break;
+                                    }
+                                }
+                                if (!achou) {
+                                    JOptionPane.showMessageDialog(null, "Produto não encontrado.");
+                                }
+                            }
+                            catch (NumberFormatException e) {
+                                JOptionPane.showMessageDialog(null, "Valor inválido.");
+                            }
+                        }
+                        break;
+
+
+
                     case 5:
+                        if (lista.isEmpty()) {
+                            JOptionPane.showMessageDialog(null, "A lista está vazia.");
+                        }
+                        else {
+                            try {
+                                int idBusca = Integer.parseInt(
+                                        JOptionPane.showInputDialog(null, "Digite o ID do produto:")
+                                );
+                                boolean achou = false;
+                                for (HashMap item : lista) {
+                                    if ((int) item.get("ID") == idBusca) {
+                                        achou = true;
+                                        int quantidadeVenda = Integer.parseInt(
+                                                JOptionPane.showInputDialog(null, "Quantidade a ser vendida:")
+                                        );
+                                        int estoqueAtual = (int) item.get("Quantidade no estoque");
+                                        if (quantidadeVenda > estoqueAtual) {
+                                            JOptionPane.showMessageDialog(null,
+                                                    "FALHA NA VENDA: Estoque insuficiente. Apenas "
+                                                            + estoqueAtual + " unidades disponíveis.");
+                                        }
+                                        else {
+                                            item.put("Quantidade no estoque", estoqueAtual - quantidadeVenda);
+                                            JOptionPane.showMessageDialog(null,
+                                                    "Venda realizada com sucesso.\n" +
+                                                            "Estoque restante: " + item.get("Quantidade no estoque"));
+                                        }
+                                        break;
+                                    }
+                                }
+                                if (!achou) {
+                                    JOptionPane.showMessageDialog(null, "Produto não encontrado.");
+                                }
+                            }
+                            catch (NumberFormatException e) {
+                                JOptionPane.showMessageDialog(null, "Valor inválido.");
+                            }
+                        }
+                        break;
+
+
+
                     case 6:
+                        if (lista.isEmpty()) {
+                            JOptionPane.showMessageDialog(null, "A lista está vazia.");
+                        }
+                        else {
+                            try {
+                                int idBusca = Integer.parseInt(
+                                        JOptionPane.showInputDialog(null, "Digite o ID do produto:")
+                                );
+                                boolean achou = false;
+                                for (int i = 0; i < lista.size(); i++) {
+                                    HashMap item = lista.get(i);
+                                    if ((int) item.get("ID") == idBusca) {
+                                        achou = true;
+                                        int estoqueAtual = (int) item.get("Quantidade no estoque");
+                                        if (estoqueAtual > 0) {
+                                            JOptionPane.showMessageDialog(null,
+                                                    "AÇÃO PROIBIDA: Não é possível excluir produtos com estoque.\n" +
+                                                            "Quantidade atual: " + estoqueAtual + ".");
+                                        }
+                                        else {
+                                            int confirmar = JOptionPane.showConfirmDialog(
+                                                    null,
+                                                    "Tem certeza que deseja excluir o produto " + item.get("Nome") + "?",
+                                                    "Confirmação",
+                                                    JOptionPane.YES_NO_OPTION
+                                            );
+                                            if (confirmar == JOptionPane.YES_OPTION) {
+                                                lista.remove(i);
+                                                JOptionPane.showMessageDialog(null, "Produto removido com sucesso.");
+                                            }
+                                        }
+                                        break;
+                                    }
+                                }
+                                if (!achou) {
+                                    JOptionPane.showMessageDialog(null, "Produto não encontrado.");
+                                }
+                            }
+                            catch (NumberFormatException e) {
+                                JOptionPane.showMessageDialog(null, "Valor inválido.");
+                            }
+                        }
+                        break;
 
 
 
@@ -112,7 +240,7 @@ public class ProjetoLoja{
                         if (lista.isEmpty()) {
                             JOptionPane.showMessageDialog(null, "A lista está vazia.");
                         }
-                       else {
+                        else {
                             boolean achou = false;
                             for (HashMap item : lista) {
                                 if ((int) item.get("Quantidade no estoque") == 0) {
@@ -125,7 +253,6 @@ public class ProjetoLoja{
                                     }
                                 }
                             }
-
                             if (!achou) {
                                 JOptionPane.showMessageDialog(null, "Não existem produtos com estoque zerado.");
                             }
@@ -155,38 +282,40 @@ public class ProjetoLoja{
                         if (lista.isEmpty()) {
                             JOptionPane.showMessageDialog(null, "A lista está vazia.");
                         }
-                       else {
+                        else {
                             try {
-                                int id = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o ID do produto:"));
+                                int idBusca = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o ID do produto:"));
                                 boolean achou = false;
                                 for (HashMap item : lista) {
-                                    if ((int) item.get("ID") == id) {
+                                    if ((int) item.get("ID") == idBusca) {
+                                        if(!achou) {
+                                            achou = true;
+                                        }
                                         double novoPreco = Double.parseDouble(JOptionPane.showInputDialog(null, "Novo preço:"));
                                         item.put("Preço", novoPreco);
                                         JOptionPane.showMessageDialog(null, "SUCESSO: O preço do produto " + item.get("Nome") +
                                                 " foi atualizado para R$ " + String.format("%.2f", novoPreco) + ".");
-                                        if(!achou) {
-                                            achou = true;
-                                        }
+                                        break;
                                     }
                                 }
-
                                 if (!achou) {
                                     JOptionPane.showMessageDialog(null, "Produto não encontrado.");
                                 }
-
+                            }
+                            catch (NullPointerException e ){
+                                JOptionPane.showMessageDialog(null, "Retornando ao menu principal.");
                             }
                             catch (NumberFormatException e) {
                                 JOptionPane.showMessageDialog(null, "Valor inválido.");
                             }
                         }
-                       break;
+                        break;
+
 
 
                     case 10:
                         JOptionPane.showMessageDialog(null, "Programa finalizado.");
                         break;
-
                 }
             }
             catch (NullPointerException e){
